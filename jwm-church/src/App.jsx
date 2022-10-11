b0f8d79f620bafdfe1f6736a3ab08d692c6d1403
AlexNgii <youngbob6711@gmail.com>
2022-10-12 00:36:32 +0300

    first commit

---
+++ jwm-church/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='bg-primary w-full overflow-hidden'>
+
+        <h1>Alex Ngii</h1>
+
+
+    </div>
+  )
+}
+
+export default App
